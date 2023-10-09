58d0dc095193f9aa491c5a441df752a347b22766
mona lisa <lizfriedman5@gmail.com>
2023-10-09 12:18:44 -0500

    import express and middleware to server

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 // import connection
 const connection = require('./config/connection')
 const { User, Thought } = require('./models')
+const routes = require('./routes')
 
 // express routing
 const express = require('express')
 const app = express()
+const port = 3001
 
+// middleware
+app.use(express.urlencoded( { extended: true } ))
+app.use(express.json())
+app.use(routes)
+
+// mongoDB connection
+const { MongoClient } = require('mongodb')
+
+// app.listen(PORT, () => {})
